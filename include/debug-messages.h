ea5c12cfc481acfb249fc49c3c07a80248a0ea21
Joshua Weinstein <jweinstein@splunk.com>
2020-01-29 01:29:09 -0800

    header dir

---
+++ include/debug-messages.h
@@ -0,0 +1,10 @@
+#ifndef MARKET_DEBUG_MESSAGES_H
+#define MARKET_DEBUG_MESSAGES_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define DEBUG_MES_EXP(exp) printf("DEBUG::exp -> '%s'", #exp)
+
+#endif // MARKET_DEBUG_MESSAGES_H
